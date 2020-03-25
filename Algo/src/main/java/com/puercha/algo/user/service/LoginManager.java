37046fa94c255d8bd78f8f3ec1ba5bb4bbd71f5d
HEY <hexo7238@gmail.com>
2020-03-25 16:30:43 +0900

    feat: User 관련 서비스 기능 추가
    LoginContoller 생성. LoginService 기능 정의, LoginManager작성. UserController생성,
    UserManager 작성. UserService 기능 정의

---
+++ Algo/src/main/java/com/puercha/algo/user/service/LoginManager.java
@@ -0,0 +1,49 @@
+package com.puercha.algo.user.service;
+
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import com.puercha.algo.user.dao.UserDAO;
+import com.puercha.algo.user.vo.UserVO;
+
+@Service
+public class LoginManager implements LoginService{
+
+	private static final Logger logger
+	= LoggerFactory.getLogger(LoginManager.class);
+	
+	@Inject
+	UserDAO userDAO;
+	// 로그인
+	@Override
+	public UserVO loginUser(String email, String pw, HttpSession session) {
+		logger.info("loginService.loginUser(String email,String pw) 호출됨");
+		UserVO user = null;
+		// 로그인 여부 확인
+		user = userDAO.selectUser(email, pw);
+		if(user!=null) {
+			//세션에 로그인된 사용자 정보 넣기 
+			session.setAttribute("userInfo", user);
+		}
+		return user;
+	}
+
+	// 로그아웃
+	@Override
+	public int logoutUser(HttpSession session) {
+		logger.info("logoutUser.logoutUser() 호출됨");
+//		session.getAttribute("userInfo");
+		UserVO user = (UserVO) session.getAttribute("userInfo");
+		if(user != null) {
+			session.removeAttribute("userInfo");
+			session.invalidate();
+			return 1;
+		}
+		return 0;
+	}
+
+}
--- Algo/src/main/java/com/puercha/algo/user/service/LoginService.java
+++ Algo/src/main/java/com/puercha/algo/user/service/LoginService.java
@@ -1,9 +1,17 @@
 package com.puercha.algo.user.service;
+
+import javax.servlet.http.HttpSession;
+
+import com.puercha.algo.user.vo.UserVO;
+
 /**
  * 사용자 로그인 관련 기능 정의
  * @author Hyeonuk
  *
  */
-public class LoginService {
-
+public interface LoginService {
+	// 로그인
+	UserVO loginUser(String email, String pw, HttpSession session);
+	// 로그아웃
+	int logoutUser(HttpSession session);
 }
