bdd0110bbacac37b7476763eefb0d4a1d19be13a
HEY <hexo7238@gmail.com>
2020-03-25 15:13:17 +0900

    feat:UserDAO 추가
    UserDAO, UserDAOImpl, UserDAO-mapper.xml을 작성함

--- Algo/src/main/java/com/puercha/algo/user/dao/UserDAO.java
+++ Algo/src/main/java/com/puercha/algo/user/dao/UserDAO.java
@@ -1,9 +1,37 @@
 package com.puercha.algo.user.dao;
+
+import java.util.List;
+
+import com.puercha.algo.user.vo.UserVO;
+
 /**
  * 사용자 데이터 DAO 정의
- * @author Hyeonuk
+ * @author Hyeonuk, Youdong
  *
  */
 public interface UserDAO {
-
+	
+	/* Create */
+	//사용자 생성
+	int insertUser (UserVO userVO);
+	
+	/* Read */
+	//사용자 전체조회
+	List<UserVO> selectAllUser();
+	//사용자 개별조회
+	UserVO selectUser(String email);	
+	UserVO selectUser(String email, String pw);
+	
+	/* Update */
+	// 사용자 수정
+	int updateUser (UserVO userVO);
+	int changePW(long userNum,String oldPw,String newPw);
+	
+	/* Delete */
+	// 사용자 삭제
+	int deleteUser (String email,String pw);
+	int deleteUser (long userNum);
+	
+	
+	
 }
