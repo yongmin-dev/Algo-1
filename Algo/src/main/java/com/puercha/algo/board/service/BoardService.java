2a65ebd67cf11ffd0d5125ad1730da6d8ec71951
yongmin <dev.min@kakao.com>
2020-03-27 17:38:44 +0900

    feat: 게시판 - BoardService 업데이트

--- Algo/src/main/java/com/puercha/algo/board/service/BoardService.java
+++ Algo/src/main/java/com/puercha/algo/board/service/BoardService.java
@@ -4,6 +4,57 @@ package com.puercha.algo.board.service;
  * @author Hyeonuk
  *
  */
+
+import java.util.List;
+import java.util.Map;
+
+import com.puercha.algo.board.vo.AttachmentVO;
+import com.puercha.algo.board.vo.BoardCategoryVO;
+import com.puercha.algo.board.vo.BoardPostVO;
+import com.puercha.algo.common.PageManager;
+
 public interface BoardService {
+	
+	List<BoardCategoryVO> getCategory();
+	
+	//게시글 작성
+	int insert(BoardPostVO boardVO);
+	
+	//게시글 수정
+	int update(BoardPostVO boardPostVO);
+	
+	//게시글삭제
+	int delete(String postNum);
+	
+	//첨부파일1건 삭제
+	int deleteFile(String fid);
+	
+	//첨부파일전체삭제
+	int deleteFiles(String postNum);
+	
+	//게시글보기
+	Map<String, Object> select(String postNum);
+	
+	//게시글 목록보기
+	//1) 전체
+	List<BoardPostVO> selectList();
+	
+	//2) 검색어 없는 게시글 페이징
+	List<BoardPostVO> selectList(int startRec, int endRec);
+	
+	//3) 검색어 있는 게시글 페이징
+	List<BoardPostVO> selectList(String reqPage, String searchType, String keyword);
+	
+	//페이지 제어
+	PageManager getPageManager(String reqPage, String searchType, String keyword);
+	
+	//게시글 답글 작성
+	
+	int insertReply(BoardPostVO boardPostVO);
+	
+	//첨부파일 조회
+	AttachmentVO selectFile(String fid);
+	
+	
 
 }
---
+++ Algo/src/main/java/com/puercha/algo/board/service/BoardServiceImpl.java
@@ -0,0 +1,213 @@
+package com.puercha.algo.board.service;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.inject.Inject;
+import javax.transaction.Transactional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.puercha.algo.board.dao.PostingDAO;
+import com.puercha.algo.board.vo.AttachmentVO;
+import com.puercha.algo.board.vo.BoardCategoryVO;
+import com.puercha.algo.board.vo.BoardPostVO;
+import com.puercha.algo.common.FindCriteria;
+import com.puercha.algo.common.PageManager;
+import com.puercha.algo.common.RowCriteria;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+	public static final Logger logger = LoggerFactory.getLogger(BoardServiceImpl.class);
+
+	@Inject
+	PostingDAO postingDAO;
+
+	// 게시글 읽어오기
+	@Override
+	public List<BoardCategoryVO> getCategory() {
+		return postingDAO.getCategory();
+	}
+
+	@Transactional
+	@Override
+	public int insert(BoardPostVO boardPostVO) {
+
+		// 1) 게시글 작성
+		int cnt = postingDAO.insert(boardPostVO);
+
+		// 2) 첨부파일 있을 경우
+		logger.info("첨부파일: " + boardPostVO.getFiles().size());
+
+		if (boardPostVO.getFiles() != null && boardPostVO.getFiles().size() > 0) {
+			insertFile(boardPostVO.getFiles(), boardPostVO.getPostNum());
+		}
+		return cnt;
+	}
+
+	// 첨부파일 저장하기
+	private void insertFile(List<MultipartFile> files, long postNum) {
+		for (MultipartFile file : files) {
+			try {
+				logger.info("파일 첨부 : " + file.getOriginalFilename());
+
+				AttachmentVO attachmentVO = new AttachmentVO();
+				// 게시글 번호
+				attachmentVO.setPostNum(postNum);
+				// 첨부파일 이름
+				attachmentVO.setFname(file.getOriginalFilename());
+				attachmentVO.setFsize(file.getSize());
+				attachmentVO.setFtype(file.getContentType());
+				attachmentVO.setFdata(file.getBytes());
+
+				if (file.getSize() > 0) {
+					postingDAO.fileInsert(attachmentVO);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	// 게시글 수정
+	@Transactional
+	@Override
+	public int update(BoardPostVO boardPostVO) {
+		// 1) 게시글 수정
+		int cnt = postingDAO.update(boardPostVO);
+		// 2) 첨부파일 추가
+		if (boardPostVO.getFiles() != null && boardPostVO.getFiles().size() > 0) {
+			insertFile(boardPostVO.getFiles(), boardPostVO.getPostNum());
+		}
+		return cnt;
+	}
+
+	// 게시글 삭제(파일 삭제 병행)
+	@Transactional
+	@Override
+	public int delete(String postNum) {
+		int cnt = 0;
+		postingDAO.deleteFiles(postNum);
+		cnt = postingDAO.delete(postNum);
+		return cnt;
+	}
+
+	// 파일 한개 삭제
+	@Transactional
+	@Override
+	public int deleteFile(String fid) {
+
+		int cnt = 0;
+		cnt = postingDAO.deleteFile(fid);
+		return cnt;
+	}
+
+	// 파일 전체 삭제
+	@Transactional
+	@Override
+	public int deleteFiles(String postNum) {
+
+		int cnt = 0;
+		cnt = postingDAO.deleteFiles(postNum);
+
+		return cnt;
+	}
+
+	@Transactional
+	@Override
+	public Map<String, Object> select(String postNum) {
+		// 1) 게시글 가져오기
+		BoardPostVO boardPostVO = postingDAO.select(postNum);
+
+		List<AttachmentVO> files = postingDAO.selectFiles(postNum);
+
+		postingDAO.updateHit(postNum);
+
+		Map<String, Object> map = new HashMap<>();
+		map.put("boardPostVO", boardPostVO);
+		if (files != null && files.size() > 0) {
+			map.put("files", files);
+		}
+
+		return map;
+	}
+
+	// 게시글 목록
+	// 1) 전체
+	@Override
+	public List<BoardPostVO> selectList() {
+		return postingDAO.selectList();
+	}
+
+	@Override
+	public List<BoardPostVO> selectList(int startRec, int endRec) {
+		return null;
+	}
+
+	@Override
+	public List<BoardPostVO> selectList(String reqPage, String searchType, String keyword) {
+
+		logger.info("list param :" + reqPage + " "+ searchType);
+		int l_reqPage = 0;
+		
+		//요청 페이지 정보가 없으면 1로 초기화
+		if(reqPage == null || reqPage.trim().isEmpty()) {
+			l_reqPage = 1;
+		}else {
+			l_reqPage = Integer.parseInt(reqPage);
+		}
+		RowCriteria rowCriteria = new RowCriteria(l_reqPage);
+		logger.info("RowCriteria"+ rowCriteria.getStartRec()+" "+ rowCriteria.getEndRec() +" " + searchType+ " " + keyword);
+		return postingDAO.selectList(rowCriteria.getStartRec(), rowCriteria.getEndRec(), searchType, keyword);
+	}
+	
+	//페이지 제어
+	@Override
+	public PageManager getPageManager(String reqPage, String searchType, String keyword) {
+		
+		PageManager pm = null;
+		FindCriteria fc = null;
+		
+		int totalRec = 0;
+		int l_reqPage = 0;
+		
+		// 요청페이지 정보가 없으면 1로 초기화
+		if(reqPage == null || reqPage.trim().isEmpty()) {
+			l_reqPage = 1;
+		}else {
+			l_reqPage = Integer.parseInt(reqPage);
+		}
+		
+		totalRec = postingDAO.countTotalRecord(searchType, keyword);
+		
+		pm = new PageManager(fc, totalRec);
+		fc = new FindCriteria(l_reqPage, searchType, keyword);
+		
+		return pm;
+	}
+	//답글 작성
+	@Transactional
+	@Override
+	public int insertReply(BoardPostVO boardPostVO) {
+		//1) 게시글 답글 작성
+		int cnt = postingDAO.insertReply(boardPostVO);
+		
+		if(boardPostVO.getFiles()!= null && boardPostVO.getFiles().size() > 0) {
+			insertFile(boardPostVO.getFiles(), boardPostVO.getPostNum());
+		}
+		return cnt;
+	}
+
+	//첨부파일 보기
+	@Override
+	public AttachmentVO selectFile(String fid) {
+		return postingDAO.selectFile(fid);
+	}
+
+}
