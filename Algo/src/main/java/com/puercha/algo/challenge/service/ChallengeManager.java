2b30552eda285406c79caeb17ef87ed7872f18bd
uginim <kimugiugi@gmail.com>
2020-04-03 16:29:27 +0900

    feat: 도전과제 목록 보기 화면 추가

--- Algo/src/main/java/com/puercha/algo/challenge/service/ChallengeManager.java
+++ Algo/src/main/java/com/puercha/algo/challenge/service/ChallengeManager.java
@@ -1,18 +1,26 @@
 package com.puercha.algo.challenge.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.inject.Inject;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
 
 import com.puercha.algo.challenge.dao.ChallengeDAO;
 import com.puercha.algo.challenge.service.codelauncher.CodeTester;
-import com.puercha.algo.challenge.service.codelauncher.JavaCodeTester;
 import com.puercha.algo.challenge.vo.ChallengeResultVO;
 import com.puercha.algo.challenge.vo.ChallengeVO;
+import com.puercha.algo.common.FindCriteria;
+import com.puercha.algo.common.PageManager;
 
+@Service
 public class ChallengeManager implements ChallengeService {
+	private static final Logger logger = LoggerFactory.getLogger(ChallengeManager.class);
 	@Inject
 	ChallengeDAO challengeDAO;
 	
@@ -21,15 +29,28 @@ public class ChallengeManager implements ChallengeService {
 	CodeTester javaCodeTester;
 	
 	@Override
-	public List<ChallengeVO> serachChallenges(long page, String keyword, String type) {
-		// TODO Auto-generated method stub
-		return null;
+	public Map<String,Object> serachChallenges(long page, String keyword, String type) {
+		Map<String,Object> searchingResult = new HashMap<String,Object> (); // 조회 결과
+		
+		// 도전과제 모든 행 개수 (검색 시 에도 같듬)
+		long totalRowNum =  challengeDAO.getCountTotalChallenges(type, keyword); // 총 개수
+		FindCriteria fc = new FindCriteria((int)page);
+		fc.setKeyword(keyword);
+		fc.setSearchType(type);
+		PageManager pageManager = new PageManager(fc, totalRowNum);// page메니저
+		searchingResult.put(KEY_PAGE_INFO, pageManager); // 결과에 추가
+		logger.info("pageManager:"+pageManager);
+		// 조회
+		List<ChallengeVO> list = challengeDAO.selectAllChallenge(fc.getStartRec(), fc.getEndRec(), type, keyword);
+		searchingResult.put(KEY_CHALLENGE_LIST, list);
+		
+		
+		return searchingResult;
 	}
 
 	@Override
-	public ChallengeVO viewChallenges(long ChallengeNum) {
-		// TODO Auto-generated method stub
-		return null;
+	public ChallengeVO viewChallenges(long challengeNum) {		
+		return challengeDAO.selectOne(challengeNum);
 	}
 
 	@Override
@@ -53,10 +74,10 @@ public class ChallengeManager implements ChallengeService {
 	 */
 	@Override
 	public ChallengeResultVO doTest(long cNum, long userNum, String code) {
+		// result데이터 생성
 		ChallengeResultVO result = createNewResult(cNum,userNum,code);
 		// 테스트 시작
-		javaCodeTester.doTest(result);
-		
+		javaCodeTester.doTest(result);		
 		return result;
 	}
 	/**
@@ -64,7 +85,7 @@ public class ChallengeManager implements ChallengeService {
 	 * @param cNum
 	 * @param userNum
 	 * @param code
-	 * @return
+	 * @return 성공 시 1
 	 */
 	private ChallengeResultVO createNewResult(long cNum, long userNum, String code) {
 		ChallengeResultVO result = new ChallengeResultVO();
